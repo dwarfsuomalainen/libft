35a8d96161b615215dbfd19ab6f383ec1585947c
rbogoudi <rbogoudi@student.hive.fi>
2023-11-22 10:52:13 +0200

    strtrim ok

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: rbogoudi <rbogoudi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:54:52 by rbogoudi          #+#    #+#             */
-/*   Updated: 2023/11/21 15:48:41 by rbogoudi         ###   ########.fr       */
+/*   Updated: 2023/11/22 10:51:59 by rbogoudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -23,31 +22,25 @@ char	*ft_strtrim(char const *s1, char const *set)
 	{
 		first++;
 	}
-	printf("\n%zu\n", first);
 	last = ft_strlen(s1);
-	printf("%d", ft_strchr(set, s1[last]));
 	while (last && ft_strchr(set, s1[last - 1]))
 	{
-		printf("\n%zu\n", last);
 		last--;
 	}
-	printf("\n%zu\n", last);
 	if (!s1 || !set)
 		return (NULL);
-
 	return (ft_substr(s1, first, last - first));
 }
 
+// int	main(void)
+// {
+// 	char    str[] = "string to be string trimmed string";
+// 	char    set[] = "string";
 
-int	main(void)
-{
-	char    str[] = "string to be string trimmed string";
-	char    set[] = "string";
-
-	if (ft_strtrim(str, set) == NULL)
-	{
-		printf("allocation fails");
-	}
-	printf("the rest of original string --> *%s*", ft_strtrim(str, set));
-	return (0);
-}
+// 	if (ft_strtrim(str, set) == NULL)
+// 	{
+// 		printf("allocation fails");
+// 	}
+// 	printf("the rest of original string --> *%s*", ft_strtrim(str, set));
+// 	return (0);
+// }
