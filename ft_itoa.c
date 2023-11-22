eee7e46ea9480fbed375f82f3df489677915165b
rbogoudi <rbogoudi@student.hive.fi>
2023-11-22 16:11:37 +0200

    split  + itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,10 +6,27 @@
 /*   By: rbogoudi <rbogoudi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:56:41 by rbogoudi          #+#    #+#             */
-/*   Updated: 2023/11/17 10:57:34 by rbogoudi         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:01:51 by rbogoudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_itoa(int n);
+char	*ft_itoa(int n)
+{
+	char	*num;
+
+	num = (char *)malloc(sizeof(char *));
+	if (!num)
+		return (NULL);
+	return (num);
+}
+
+int	main(void)
+{
+	int	n;
+
+	n = 0;
+	printf("string representation of number %d is %s", n, ft_itoa(n));
+	return (0);
+}
--- ft_split.c
+++ ft_split.c
@@ -6,30 +6,12 @@
 /*   By: rbogoudi <rbogoudi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:55:49 by rbogoudi          #+#    #+#             */
-/*   Updated: 2023/11/22 14:59:25 by rbogoudi         ###   ########.fr       */
+/*   Updated: 2023/11/22 15:29:29 by rbogoudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-
-// char	**ft_split(char const *s, char c)
-// {
-// 	char	**arr;
-
-// 	printf("\n*%c*\n", c);
-// 	if (!arr)
-// 		return (NULL);
-// 	return (arr);
-// }
-
-// int	main(void)
-// {
-// 	char    str[] = "this string to be splitted";
-	
-// 	printf("array of strings after splitting this strind %s --> %s", str, ft_split(str, " "));
-// 	return (0);
-// }
 char	**ft_split(char const *s, char c)
 {
 	char	**arr;
@@ -41,35 +23,20 @@ char	**ft_split(char const *s, char c)
 	j = 0;
 	start = 0;
 
-	// Allocate memory for the array
 	arr = (char **)malloc(sizeof(char *) * (ft_strlen(s) + 1));
 	if (!arr)
 		return (NULL);
-
-	// Loop through the string
 	while (s[i])
 	{
-		// Find the beginning of a word
 		while (s[i] == c)
 			i++;
-
-		// Mark the start of the word
 		start = i;
-
-		// Find the end of the word
 		while (s[i] && s[i] != c)
 			i++;
-
-		// Allocate memory for the word
 		arr[j] = ft_substr(s, start, i - start);
-
-		// Move to the next index
 		j++;
 	}
-
-	// Set the last element of the array to NULL
 	arr[j] = NULL;
-
 	return (arr);
 }
 
@@ -79,8 +46,6 @@ int	main(void)
 	char	**result;
 
 	result = ft_split(str, ' ');
-
-	// Print the array of strings
 	while (*result)
 	{
 		printf("%s\n", *result);
