685dd40cbab0b018abd27e2f2b4f655e94b97d2c
rbogoudi <rbogoudi@student.hive.fi>
2023-11-23 12:27:20 +0200

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,19 +6,55 @@
 /*   By: rbogoudi <rbogoudi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/17 10:56:41 by rbogoudi          #+#    #+#             */
-/*   Updated: 2023/11/22 16:01:51 by rbogoudi         ###   ########.fr       */
+/*   Updated: 2023/11/23 12:20:41 by rbogoudi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static int	ft_intlen(int n)
+{
+	int	intlen;
+
+	intlen = 0;
+	if (n == 0)
+		return (1);
+	if (n < 0)
+		n = -n;
+	while (n != 0)
+	{
+		n /= 10;
+		intlen++;
+	}
+	printf("%d", intlen);
+	return (intlen);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*num;
+	int		isneg;
+	int		length;
+	int		index;
 
-	num = (char *)malloc(sizeof(char *));
+	if (n < 0)
+		isneg = 1;
+	else
+		isneg = 0;
+	length = ft_intlen(n);
+	num = (char *)malloc(length + isneg + 1);
 	if (!num)
 		return (NULL);
+	index = (length + isneg) - 1;
+	num[index + 1] = '\0';
+	if (isneg)
+		num[0] = '-';
+	n = -n;
+	while (index >= 0 && n != 0)
+	{
+		num[index--] = '0' + n % 10;
+		n /= 10;
+	}
 	return (num);
 }
 
@@ -26,7 +62,7 @@ int	main(void)
 {
 	int	n;
 
-	n = 0;
+	n = -666999888;
 	printf("string representation of number %d is %s", n, ft_itoa(n));
 	return (0);
 }
